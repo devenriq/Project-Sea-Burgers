23a7989a2f5e65741dcb21513ccd878cba3086b7
Enrique <octepc.97@gmail.com>
2022-08-11 16:57:17 -0500

    first coment

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      primary: "#f3c614",
+      secondary: "#353535",
+    },
+  },
+  plugins: [],
+};
