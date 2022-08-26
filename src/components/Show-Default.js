161c6bc51e18dc85f4e34ab3a6917d4279cc3310
Enrique <octepc.97@gmail.com>
2022-08-26 12:03:04 -0500

    simulated a consumnation of API using setTimeOut

--- src/components/Show-Default.js
+++ src/components/Show-Default.js
@@ -17,62 +17,68 @@ export class CreateNewBurger {
 
 // Showing the burgers in the layout
 export const showBurgers = () => {
-  listOfBurgers.forEach((burger) => {
-    const burgerContainer = document.createElement("div");
-    burgerContainer.classList.add("burger-container");
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(
+        listOfBurgers.forEach((burger) => {
+          const burgerContainer = document.createElement("div");
+          burgerContainer.classList.add("burger-container");
 
-    const imgContainer = document.createElement("div");
-    imgContainer.classList.add("md:w-1/2");
-    const image = document.createElement("img");
-    image.classList.add("burger-image");
-    image.src = burger.image;
+          const imgContainer = document.createElement("div");
+          imgContainer.classList.add("md:w-1/2");
+          const image = document.createElement("img");
+          image.classList.add("burger-image");
+          image.src = burger.image;
 
-    const textContainer = document.createElement("div");
-    textContainer.classList.add("md:w-1/2");
-    textContainer.classList.add("container-text");
-    textContainer.setAttribute("id", "container-text");
+          const textContainer = document.createElement("div");
+          textContainer.classList.add("md:w-1/2");
+          textContainer.classList.add("container-text");
+          textContainer.setAttribute("id", "container-text");
 
-    const textTitle = document.createElement("h2");
-    textTitle.classList.add("burger-name");
-    const name = document.createTextNode(burger.name);
-    textTitle.append(name);
+          const textTitle = document.createElement("h2");
+          textTitle.classList.add("burger-name");
+          const name = document.createTextNode(burger.name);
+          textTitle.append(name);
 
-    const textP = document.createElement("p");
-    textP.classList.add("burger-description");
-    const description = document.createTextNode(burger.description);
-    textP.append(description);
+          const textP = document.createElement("p");
+          textP.classList.add("burger-description");
+          const description = document.createTextNode(burger.description);
+          textP.append(description);
 
-    const burgerPrice = document.createElement("div");
-    burgerPrice.classList.add("burger-price");
-    const price = document.createTextNode(`$ ${burger.price}`);
-    burgerPrice.append(price);
+          const burgerPrice = document.createElement("div");
+          burgerPrice.classList.add("burger-price");
+          const price = document.createTextNode(`$ ${burger.price}`);
+          burgerPrice.append(price);
 
-    // const burgerPunctuation = document.createElement("div");
-    // burgerPunctuation.classList.add("burger-punctuation");
-    // const punctuation = document.createTextNode(`${burger.punctuation}`);
-    // burgerPunctuation.append(punctuation);
+          // const burgerPunctuation = document.createElement("div");
+          // burgerPunctuation.classList.add("burger-punctuation");
+          // const punctuation = document.createTextNode(`${burger.punctuation}`);
+          // burgerPunctuation.append(punctuation);
 
-    buyButton = document.createElement("button");
-    buyButton.classList.add("btn-buy");
-    const buy = document.createTextNode("Buy");
-    buyButton.append(buy);
+          buyButton = document.createElement("button");
+          buyButton.classList.add("btn-buy");
+          const buy = document.createTextNode("Buy");
+          buyButton.append(buy);
 
-    imgContainer.append(image);
-    textContainer.append(
-      textTitle,
-      textP,
-      burgerPrice,
-      // burgerPunctuation,
-      buyButton
-    );
-    burgerContainer.append(imgContainer, textContainer);
+          imgContainer.append(image);
+          textContainer.append(
+            textTitle,
+            textP,
+            burgerPrice,
+            // burgerPunctuation,
+            buyButton
+          );
+          burgerContainer.append(imgContainer, textContainer);
 
-    listBurgers.append(burgerContainer);
-    buyButton.addEventListener("click", formAlert);
+          listBurgers.append(burgerContainer);
+          buyButton.addEventListener("click", formAlert);
 
-    // const newBurger = `
+          // const newBurger = `
 
-    // `;
-    // listBurgers.innerHTML += newBurger;
+          // `;
+          // listBurgers.innerHTML += newBurger;
+        })
+      );
+    }, 2000);
   });
 };
